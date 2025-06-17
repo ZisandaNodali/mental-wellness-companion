689efd43ad79b7f993d24884cfd670ccc6db9053
Zisanda Nodali <zisandanodali01@gmail.com>
2025-06-17 15:09:02 +0200

    Added a .env file

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCmrQP2L4wiWAmHwfVAfy0wy73WfS5mxqU",
-  authDomain: "mental-wellness-companio-4654a.firebaseapp.com",
-  projectId: "mental-wellness-companio-4654a",
-  storageBucket: "mental-wellness-companio-4654a.firebasestorage.app",
-  messagingSenderId: "243115454133",
-  appId: "1:243115454133:web:87f579096e97256c4818fc",
-  measurementId: "G-HBT0JHKDS8"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
